f517dbb50c8d65704c6eb5acda11be2a0cf0547e
Rafael Magaña <raismaav.2510@gmail.com>
2022-11-08 22:01:56 -0600

    Soporte para configurar IP

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,13 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Servidor {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String ip = scanner.nextLine();
         try {
-            System.setProperty("java.rmi.server.hostname","192.168.1.112");
+            System.setProperty("java.rmi.server.hostname", ip);
             Registry registry = LocateRegistry.createRegistry(8080);
             registry.rebind("calculadora", new Implementacion());
             System.out.println("Servidor iniciado");
