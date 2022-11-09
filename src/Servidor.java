e18a8e53431357d5eea3dfca19a9ad7b9e63e886
Rafael Magaña <raismaav.2510@gmail.com>
2022-11-09 15:11:14 -0600

    Textos corregidos

--- src/Servidor.java
+++ src/Servidor.java
@@ -12,12 +12,13 @@ import java.util.logging.Logger;
 public class Servidor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa la dirección IP del servidor: ");
         String ip = scanner.nextLine();
         try {
             System.setProperty("java.rmi.server.hostname", ip);
             Registry registry = LocateRegistry.createRegistry(8080);
-            registry.rebind("calculadora", new Implementacion());
-            System.out.println("Servidor iniciado");
+            registry.rebind("Calculadora", new Implementacion());
+            System.out.println("Servidor iniciado con la IP: " + ip);
         } catch (RemoteException ex) {
             Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
         } catch (Exception ex) {
